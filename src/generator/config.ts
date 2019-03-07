e05c695497149b3542955a2cab3494bc2cebc36d
Theo Sun <theo.sun@sap.com>
2019-03-07 16:21:43 +0800

    chore: refactor generator

---
+++ src/generator/config.ts
@@ -0,0 +1,36 @@
+
+/**
+ * Table Config
+ */
+export interface TableConfig {
+
+  columnsCount: number;
+
+  indexsCount: number;
+
+  rowsCount: number;
+
+}
+
+/**
+ * Database Config
+ */
+export interface DatabaseConfig {
+
+  tablesCount: number;
+
+}
+
+export interface GeneratedDataBase {
+
+
+
+}
+
+export interface GeneratedTable {
+
+}
+
+export interface GeneratedColumn {
+  
+}
